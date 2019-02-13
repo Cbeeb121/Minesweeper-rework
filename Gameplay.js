345cb756c8e30faff58e4f492a7412f793a2f39b
John Quitno <john.quitno@ku.edu>
2019-02-12 21:02:47 -0600

    added the win function

--- Gameplay.js
+++ Gameplay.js
@@ -1,1 +1,19 @@
-
+function win(rows, cols, arr)
+{
+    let count = 0;
+    for(let i = 0; i < cols; i++)
+    {
+        for(let j = 0; j < rows; j++)
+        {
+            if(!arr[i][j].isUnCovered())
+            {
+                count++;
+            }
+        }
+    }
+    if(count==numOfBombs)
+    {
+        return true;
+    }
+    return false;
+}
