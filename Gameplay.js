af5f60792a9d789489827c775e42e1dbda103f4a
giangpro93 <giangnt93@ku.edu>
2019-02-14 08:49:35 -0600

    Implement recursive function

--- Gameplay.js
+++ Gameplay.js
@@ -1,11 +1,19 @@
 var gamepalay = {
+    var dx = [-1,-1,0,1,1,1,0,-1];
+    var dy = [0,1,1,1,0,-1,-1,-1];
+    var mark = [];
+
     start: function()
     {
-        var rows = $('rows').value,
-            cols = $('cols').value,
-            mines = $('mines').value
+        this.rows = $('rows').value;
+        this.cols = $('cols').value;
+        this.mines = $('mines').value;
 
-        let myBoard = new Board(rows,cols,mines);
+        this.myBoard = new Board(rows,cols,mines);
+
+        // not sure if needed
+        for(let i=0;i<rows;i++) mark[i]=[];
+
         //onclick stuff
 
     },
@@ -16,15 +24,37 @@ var gamepalay = {
         myBoard.flag(row,col);
     },
 
+    isInside: function(row, col)
+    {
+        return(row>=0 && col>=0 && row<rows && col<cols);
+    },
+
+    revealHelper: function(row, col)
+    {
+        mark[row,col]=false;
+        for(let i=1;i<=8,i++) {
+            let u = row+dx[i];
+            let v = col+dy[i];
+            if (isInside(u,v) && mark[u][v]) {
+                myBoard.reveal(u,v);
+                if (myBoard.getNumber(u,v)==0) revealHelper(u,v);
+            }
+        }
+    },
+
     leftClick: function(row,col)
     {
         if(myBoard.isMine(row,col))
         {
-            gameplay.lose();
+            gameplay.lose(row,col);
         }
         else if(myBoard.getNumber(row,col)==0)
         {
+            // initiate mark array
+            for(let i=0;i<rows;i++)
+            for(let j=0;j<cols;j++) mark[i][j]=true;
             //recursive stuff
+            revealHelper(row,col);
         }
         else
         {
@@ -32,7 +62,7 @@ var gamepalay = {
             myBoard.checkWin();
         }
     },
-    
+
     checkWin: function()
     {
         if(myBoard.numRevealed==(rows*cols-mines))
@@ -43,7 +73,7 @@ var gamepalay = {
     }
 }
 
-window.onload = function () 
+window.onload = function ()
 {
     gameplay.start();
 }
